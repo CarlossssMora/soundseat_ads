6d8d746767c3119f56d98fdea3e53ca9b73493bc
CarlossssMora <juanmorais.05@gmail.com>
2024-11-15 16:10:28 -0600

    Plantillas react

--- front_end/soundseat/src/App.js
+++ front_end/soundseat/src/App.js
@@ -4,6 +4,10 @@ import './App.css';
 function App() {
   return (
     //Dentro de este div se pone todo el código HTML
+    // Para utilizar un componente hay que importarlo primero
+    //  import Componente from 'direccion';
+    // Para utilizarlo, se mete el componente dentro de div
+    // <Componente />
     <div className="App">
       
     </div>
---
+++ front_end/soundseat/src/componentes/footer.js
@@ -0,0 +1,14 @@
+//Necesario en todos los componentes
+import React from 'react';
+//Importar para aplicar los estilos al componente
+import '../stylesheets/footer.css'
+
+
+function Componente(){
+    return(
+        <h1>Hola</h1>
+        //Código HTML
+    );
+}
+
+export default Componente;
